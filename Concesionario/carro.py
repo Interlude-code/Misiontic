410710e2451ec33ea8d9faaddf6d886bf2898f5b
iSebastianCabrera <85758073+iSebastianCabrera@users.noreply.github.com>
2021-06-11 16:53:28 -0500

    hola

--- Concesionario/carro.py
+++ Concesionario/carro.py
@@ -10,5 +10,5 @@ class carro(vehiculos):
         self.siGas=siGas
         self.siHybrido=siHybrido
 
-    sebastian pudiste ayudaaaa nada sssa
+    ##sebastian pudiste ayudaaaa nada sssa bhjhjh 
 
