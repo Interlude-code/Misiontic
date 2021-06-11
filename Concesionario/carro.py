c4a6b04f92759da453cccf4e7415b9dffcb025ef
iSebastianCabrera <85758073+iSebastianCabrera@users.noreply.github.com>
2021-06-11 13:54:10 -0500

    updated

--- Concesionario/carro.py
+++ Concesionario/carro.py
@@ -1,9 +1,15 @@
 from vehiculos import vehiculos
 
+
+
+
 class carro(vehiculos):
     def __init__(self, kilometraje, modelo, precio,inventario,siGas,siHybrido,marca): 
         super().__init__(kilometraje, modelo, precio,inventario)
         self.marca=marca
         self.siGas=siGas
         self.siHybrido=siHybrido
+
+        pruebas
+
 
