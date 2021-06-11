cf50b85589f27a3f1062703c1758aca201a44829
iSebastianCabrera <85758073+iSebastianCabrera@users.noreply.github.com>
2021-06-11 12:28:00 -0500

    Delete carro.py

--- carro.py
+++
@@ -1,9 +0,0 @@
-from vehiculos import vehiculos
-
-class carro(vehiculos):
-    def __init__(self, kilometraje, modelo, precio,inventario,siGas,siHybrido,marca): 
-        super().__init__(kilometraje, modelo, precio,inventario)
-        self.marca=marca
-        self.siGas=siGas
-        self.siHybrido=siHybrido
-
