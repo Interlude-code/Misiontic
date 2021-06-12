b8af57fa817ac805ad0352986db11c7fad92be08
iSebastianCabrera <85758073+iSebastianCabrera@users.noreply.github.com>
2021-06-11 21:35:42 -0500

    update

--- Proyecto django/proyecto1/views.py
+++ Proyecto django/proyecto1/views.py
@@ -3,14 +3,18 @@ import datetime
 from django.template import Template,Context # añadir el import para usar Templat y Context
 
 def saludo(request): #primera vista
+    nombre='Juan Carlos Cabrera Pinto'
+    ahora=datetime.datetime.now()
     doc_open=open("C:/Users/Interlude/Desktop/Primer sitio web html/django\Misiontic/plantillas html/plantillaPagina.html")#abre el archivo donde esta el codigo html
     plt=Template(doc_open.read())##se lo lleva el objeto Templte el archivo se debe usar .red
     doc_open.close()#se cierra el archivo
-    ctx= Context()#se crea el objeto Context si hay dinamicidad en la pagina en este caso sera vacio
+    ctx= Context({'nombrePersona':nombre,'dia':ahora})#se crea el objeto Context si hay dinamicidad en la pagina recibe diccionarios y objetos como entrada
     documento=plt.render(ctx)#se renderiza la pagina con .render la entrada de render sera el Context
     return HttpResponse(documento)
+
 def despedida(request):
     return HttpResponse("adios django")
+
 def fecha(request):
     fecha_actual=datetime.datetime.now()
     documento='''<html>
@@ -21,6 +25,7 @@ def fecha(request):
     <body/>
     <html/>''' % fecha_actual
     return HttpResponse(documento)
+    
 def calcularEdad(request,edad, anio):
     periodo= anio-2021
     edadFutra = edad + periodo
