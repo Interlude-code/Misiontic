97b83d8b3a65922e6104297c6ad3760d4634f52c
iSebastianCabrera <85758073+iSebastianCabrera@users.noreply.github.com>
2021-06-12 08:00:10 -0500

    plantillas cambio render

--- Proyecto django/proyecto1/views.py
+++ Proyecto django/proyecto1/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 import datetime
-from django.template import Template,Context,loader # añadir el import para usar Templat y Context
-
+from django.template import Template,Context # añadir el import para usar Templat y Context
+from django.template.loader import get_template
+from django.shortcuts import render
 def saludo(request): #primera vista
     nombre='caramela'
     ahora=datetime.datetime.now()
@@ -9,10 +10,11 @@ def saludo(request): #primera vista
     #doc_open=open("C:/Users/Interlude/Desktop/Primer sitio web html/django\Misiontic/plantillas html/plantillaPagina.html")#abre el archivo donde esta el codigo html
     #plt=Template(doc_open.read())##se lo lleva el objeto Templte el archivo se debe usar .red
     #doc_open.close()#se cierra el archivo
-    doc_open=loader.get_template('plantillaPagina.html')
+    #doc_open=get_template('plantillaPagina.html')
     #ctx= Context({'nombrePersona':nombre,'dia':ahora,'gatos':lista1})#se crea el objeto Context si hay dinamicidad en la pagina recibe diccionarios y objetos como entrada
-    documento=doc_open.render({'nombrePersona':nombre,'dia':ahora,'gatos':lista1})#se renderiza la pagina con .render la entrada de render sera el Context
-    return HttpResponse(documento)
+    #documento=doc_open.render({'nombrePersona':nombre,'dia':ahora,'gatos':lista1})#se renderiza la pagina con .render la entrada de render sera el Context
+    return render(request,'plantillaPagina.html',{'nombrePersona':nombre,'dia':ahora,'gatos':lista1}) #hace todo simplificado el request es obligatorio ,el siguiente es
+                                                                                                        #la plantilla a renderizar ojo se debe tener en settings la carpeta y luego el diccionario del context y asi estaria todo 
 
 def despedida(request):
     return HttpResponse("adios django")
