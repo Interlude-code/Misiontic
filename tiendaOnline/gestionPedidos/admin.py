3ef9aaaacab241429493beb4a6143813bb504bf5
iSebastianCabrera <85758073+iSebastianCabrera@users.noreply.github.com>
2021-06-19 11:02:17 -0500

    uso de paginas de administracion

--- tiendaOnline/gestionPedidos/admin.py
+++ tiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from gestionPedidos.models import carros,clientes
 
 # Register your models here.
+class clientesAdmin(admin.ModelAdmin):
+    list_display=("nombre","documento")
+    search_fields=('documento','email')
+class carrosAdmin(admin.ModelAdmin):
+    list_display=("placa",)
+    search_fields=('placa',)
+    list_filter=("marca","fechaI")
+
+admin.site.register(carros,carrosAdmin)
+admin.site.register(clientes,clientesAdmin)
--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -3,14 +3,22 @@ from django.db import models
 class clientes(models.Model):
         nombre=models.CharField(max_length=70)
         direccion=models.CharField(max_length=50)
-        email=models.EmailField()
+        email=models.EmailField(blank=True, null=True)
         telefono=models.CharField(max_length=10)
         documento=models.IntegerField()
+        #cliente1=clientes(nombre='',direccion='',email='',telefono='',documento='')
+        def __str__(self):
+                return self.nombre
 class carros(models.Model):
     kilometraje=models.CharField(max_length=20)
-    modelo=models.CharField(max_length=4)
+    modelo=models.CharField(max_length=4,verbose_name='Año de fabricacion')
     precio=models.IntegerField()
     inventario=models.IntegerField()
-    siGas=models.BooleanField()
-    siHybrido=models.BooleanField()
     marca=models.CharField(max_length=10)
+    placa=models.CharField(max_length=10)
+    fechaI=models.DateField(blank=True, null=True)
+#carr1=carros(kilometraje='20000',modelo='corsa',precio=15000000,inventario=1,siGas=False,siHybrido=False,marca='renoult',placa='MIE88')       
+    
+
+    def __str__(self):
+            return self.placa
